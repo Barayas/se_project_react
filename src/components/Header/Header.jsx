5b7b2dd8c1cadcf262c27c2366384432d8815ef6
Nathan <nathaniellouie42@gmail.com>
2025-06-20 13:20:43 -0500

    Add modal component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,18 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 
-function Header() {
+function Header({ handleAddGarment }) {
   return (
     <header className="header">
       <img src={logo} alt="wtwr" className="header__logo"></img>
       <p className="header__date">DATE, LOCATION</p>
-      <button className="header__add-btn">+ ADD CLOTHES</button>
+      <button
+        type="button"
+        onClick={handleAddGarment}
+        className="header__add-btn"
+      >
+        + Add clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
