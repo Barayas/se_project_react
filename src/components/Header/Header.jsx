8732a61fc3f9461948b6621c3d45f1ab9a945ecb
Nathan <nathaniellouie42@gmail.com>
2025-06-19 21:59:52 -0500

    Set up basic header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src={logo} alt="wtwr" className="header__logo"></img>
+      <p className="header__date">DATE, LOCATION</p>
+      <button className="header__add-btn">+ ADD CLOTHES</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
